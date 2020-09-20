223be0ad47099addbe3068baaceb3ebb55fa08fc
g1.kwon <spring_shot@naver.com>
2020-09-20 22:17:30 +0900

    add Member Service

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        //EXCEPTION
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("Already Existed");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
