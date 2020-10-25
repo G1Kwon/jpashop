bfaf0ddbd1365878be46d905725fd195cc640084
g1.kwon <spring_shot@naver.com>
2020-10-25 20:36:24 +0900

    add 회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //@Controller + @ResponseBody = @RestController
 @RestController
@@ -15,6 +17,34 @@ import javax.validation.Valid;
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMember();
+    }
+
+    @GetMapping("api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMember();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
